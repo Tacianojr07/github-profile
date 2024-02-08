2d945861b57e5009cca9ac3e7c145716e4457e70
Tacianjr07 <t.juniior13@gmail.com>
2024-02-08 12:19:20 -0300

    :sparkles: feat: header structure html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import search from "./assets/Search.svg";
+import planet from "./assets/planet.png";
+
 function App() {
   return (
-    <div>
-      <div>HELLO WORLD</div>
+    <div className="container">
+      <header>
+        <img src={planet} alt="" />
+        <div>
+          <img src={search} alt="svg search" />
+          <input type="text" />
+        </div>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/123975416?s=96&v=4"
+            alt="photo github"
+          />
+          <div>
+            <span>GitHub</span>
+            <p>How people build software.</p>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
